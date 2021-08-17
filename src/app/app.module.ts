fce23d75950a37fbbae3ac352851b971a8c4ab0c
eng-investor.com <admin@eng-investor.com>
2021-08-17 15:44:12 +0900

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ChartsModule } from 'ng2-charts';
 
 import { AppComponent } from './app.component';
+import { DoughnutChartComponent } from './doughnut-chart/doughnut-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DoughnutChartComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
